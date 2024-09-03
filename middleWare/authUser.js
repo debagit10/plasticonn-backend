4170c351484267974dfe869390bf2116569b789a
Josiah Elias <josiahelias11@gmail.com>
2024-09-03 13:18:08 +0100

    fix: client url added

--- middleWare/authUser.js
+++ middleWare/authUser.js
@@ -2,19 +2,20 @@ const { verifyToken, decryptToken } = require("../config/token");
 
 const authUser = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-
   const token = authHeader && authHeader.split(" ")[1];
+
+  const clientURL = process.env.CLIENT_URL;
 
   try {
     if (!token) {
-      return res.redirect("/landing-page"); // Redirect to the landing page if no token is provided
+      return res.redirect(clientURL); // Redirect to the landing page if no token is provided
     }
 
     const decryptedToken = decryptToken(token);
     const verificationResult = verifyToken(decryptedToken);
 
     if (verificationResult.error || verificationResult.valid === false) {
-      return res.redirect("/landing-page"); // Redirect to the landing page if the token is invalid or expired
+      return res.redirect(clientURL); // Redirect to the landing page if the token is invalid or expired
     }
 
     // Store the decrypted token in the req object so the next route can access it
@@ -24,7 +25,7 @@ const authUser = (req, res, next) => {
     next();
   } catch (error) {
     console.error("Authentication error:", error);
-    res.redirect("/landing-page"); // Redirect to the landing page if there's a server error
+    res.redirect(clientURL); // Redirect to the landing page if there's a server error
   }
 };
 
