861d287ccc3fcf7420baa21c1bdbeaeb05fa7716
Josiah Elias <josiahelias11@gmail.com>
2024-09-03 13:31:27 +0100

    fix: checkActiveAccount import

--- routes/dropRoute.js
+++ routes/dropRoute.js
@@ -5,7 +5,7 @@ const {
   manageDrop,
 } = require("../controllers/dropControllers");
 const authUser = require("../middleWare/authUser");
-const checkActiveAccount = require("../middleware/checkActiveAccount");
+const checkActiveAccount = require("../middleware/checkActiveAccount.js");
 
 router.post("/add", authUser, checkActiveAccount, drop);
 router.get("/view", authUser, checkActiveAccount, viewDrop);
