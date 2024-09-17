fdbd836cd1f5ea4a7627724ad8fd94fed1580bca
Josiah Elias <josiahelias11@gmail.com>
2024-09-17 17:09:46 +0100

    feat: added coordinates field to the drop off center model

--- models/dropOffCenterModel.js
+++ models/dropOffCenterModel.js
@@ -4,6 +4,7 @@ const dropOffCenterSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
     address: { type: String, required: true },
+    coordinates: { type: [Number], required: true },
     person: { type: String, required: true },
     phone: { type: String, required: true },
     email: { type: String, required: true, unique: true },
